1973880c8d8eebbbc20c597351d02a09ee7094fe
marlonruffo <ruffomarlon9@gmail.com>
2023-02-15 01:28:24 -0300

    Navbar responsiva card arrumado

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import { Navbar1 } from '../src/components/Navbar'
 import { Footer1 } from '../src/components/Footer'
+import { CardContato1 } from '../src/components/Contato'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function HomePage() {
   return (
     <div>
     <Navbar1 NomeDoSite={'Cloud Service'} Item1={'Home'} Item2={'Contato'} Item3={'Funcionários'} Item4={'Login'} Logo={'/logonav.png'}/>
+    <CardContato1 Titulo={'alallala'} Imagem1={'/instagram.png'} Imagem2={'/instagram.png'} Imagem3={'/instagram.png'} Imagem4={'/instagram.png'} LabelImagem1={'eai'} LabelImagem2={'eai'} LabelImagem3={'eai'} LabelImagem4={'eai'}/>
     <Footer1 LogoEmpresa={'/cloudservice.png'} EscritaCentro={'Italian Cuisine'} LogoRedeSocial1={'/instagram.png'} LogoRedeSocial2={'/facebook.png'} LogoRedeSocial3={'/linkedin.png'} />
   
     </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,49 +1,66 @@
 
-import NextImage from 'next/image'
-interface NavbarProps{
-    NomeDoSite:string
-    Item1:string
-    Item2:string
-    Item3:string
-    Item4:string
-    Logo:string
-}
+import NextImage from "next/image";
 
-export function Navbar1({ Logo, NomeDoSite, Item1, Item2, Item3, Item4  }: NavbarProps){
-    return(
-        <div className='Nav-container'>
-            <nav>
-                <div className="NavEsquerda">
-                    <ul>
-                    <NextImage className="logonavbar" src={Logo} alt={'Logo da Marca'} width={'50'} height={'50'}/> 
-                        <li className="NomeDoSite">{NomeDoSite}</li>
-                    </ul>
-                </div>
-                <div className="NavDireita">              
-                    <ul>
-                    <li className=" nav-item">
-                            <a className="Home" href='#'>
-                                {Item1}
-                            </a>
-                        </li>
-                        <li className=" nav-item">
-                            <a className="nav-link" href='#'>
-                                {Item2}
-                            </a>
-                        </li>
-                        <li className=" nav-item">
-                            <a className="nav-link" href='#'>
-                                {Item3}
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className=" NavLogin" href='#'>
-                                {Item4}
-                            </a>
-                        </li>
-                    </ul>
-                </div>            
-            </nav>
+type NavbarProps = {
+  Logo: string;
+  NomeDoSite: string;
+  Item1: string;
+  Item2: string;
+  Item3: string;
+  Item4: string;
+};
+
+export function Navbar1({
+  Logo,
+  NomeDoSite,
+  Item1,
+  Item2,
+  Item3,
+  Item4,
+}: NavbarProps) {
+  
+
+
+
+  return (
+    <div className="Nav-container">
+      <nav>
+        <div className="NavEsquerda">
+          <ul>
+            <li className="logonavbar">
+              <NextImage src={Logo} alt={"Logo da Marca"} width={80} height={80} />
+            </li>
+            <li className="NomeDoSite">{NomeDoSite}</li>
+          </ul>
         </div>
-    )
+        <div className="NavDireita">
+          <div className="NavLinks">
+            <ul>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {Item1}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {Item2}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {Item3}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="NavLogin" href="#">
+                  {Item4}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+    </div>
+  );
 }
