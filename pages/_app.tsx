87e4d37e1dbe5b4d7719f7a5700c7581729367a0
marlonruffo <ruffomarlon9@gmail.com>
2023-02-16 04:59:53 -0300

    Atualizações titulo home e imagens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/login.css'
 import '../styles/contato.css'
 import '../styles/footer.css'
 import '../styles/cardcomidaprincipal.css'
+import '../styles/home.css'
 
 
 export default function App({ Component, pageProps }) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,19 @@ import { CardComidaPrincipal1 } from '../src/components/CardComidaPrincipal'
 import { CardComidaPrincipal2 } from '../src/components/CardComidaPrincipal'
 import { CardContato1 } from '../src/components/Contato'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
   return (
     <div>
     <Navbar1 NomeDoSite={'Cloud Service'} Item1={'Home'} Item2={'Contato'} Item3={'Funcionários'} Item4={'Login'} Logo={'/logonav.png'}/>
-    <CardComidaPrincipal1 NomeDoPrato1={'Carbonara'} DescricaoDoPrato1={'Spaghetti ao molho cremoso de ovo, queijo pecorino romano e bacon crocante. Uma receita clássica da culinária italiana que equilibra perfeitamente os sabores adocicados e salgados para um prato satisfatório.'} ImagemDoPrato1={'/pizza1.png'}/>
-    <CardComidaPrincipal2 NomeDoPrato2={'La Pulcinella'} DescricaoDoPrato2={'Spaghetti ao molho de tomate fresco e manjericão, acompanhado de camarões crocantes empanados e fritos. Um prato equilibrado e saboroso, perfeito para quem gosta de frutos do mar e combinações inusitadas.'} ImagemDoPrato2={'/pizza1.png'}/>
+    <h1 className='TitlesHome'>Pratos do dia</h1>
+    <CardComidaPrincipal1 NomeDoPrato1={'Carbonara'} DescricaoDoPrato1={'Spaghetti ao molho cremoso de ovo, queijo pecorino romano e bacon crocante. Uma receita clássica da culinária italiana que equilibra perfeitamente os sabores adocicados e salgados para um prato satisfatório.'} ImagemDoPrato1={'/carbonara1.jpg'}/>
+    <CardComidaPrincipal2 NomeDoPrato2={'La Pulcinella'} DescricaoDoPrato2={'Spaghetti ao molho de tomate fresco e manjericão, acompanhado de camarões crocantes empanados e fritos. Um prato equilibrado e saboroso, perfeito para quem gosta de frutos do mar e combinações inusitadas.'} ImagemDoPrato2={'/LaPulcinella.jpg'}/>
     <CardComidaPrincipal1 NomeDoPrato1={'Pizza'} DescricaoDoPrato1={'Pizza gourmet feita com massa fina e crocante, recheada  com cogumelos frescos, presunto Parma, e queijos especiais. É um prato perfeito para quem gosta de experimentar novos sabores.'} ImagemDoPrato1={'/pizza1.png'}/>
-    <CardComidaPrincipal2 NomeDoPrato2={'Risoto'} DescricaoDoPrato2={'Risoto cremoso de frutos do mar, feito com arroz arbóreo e um mix de frutos do mar,Finalizado com um toque de queijo parmesão e ervas frescas. É um prato perfeito para quem ama frutos do mar.'} ImagemDoPrato2={'/pizza1.png'}/>
+    <CardComidaPrincipal2 NomeDoPrato2={'Risoto'} DescricaoDoPrato2={'Risoto cremoso de frutos do mar, feito com arroz arbóreo e um mix de frutos do mar,Finalizado com um toque de queijo parmesão e ervas frescas. É um prato perfeito para quem ama frutos do mar.'} ImagemDoPrato2={'/risoto.jpg'}/>
+    <h1 className='TitlesHome'>Bebidas</h1>
     <Footer1 LogoEmpresa={'/cloudservice.png'} EscritaCentro={'Italian Cuisine'} LogoRedeSocial1={'/instagram.png'} LogoRedeSocial2={'/facebook.png'} LogoRedeSocial3={'/linkedin.png'} />
   
     </div>
