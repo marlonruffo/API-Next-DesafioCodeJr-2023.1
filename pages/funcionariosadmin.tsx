f8244e65520fd85dd872e19e41e48f9016b50946
marlonruffo <ruffomarlon9@gmail.com>
2023-03-02 16:32:12 -0300

    tabela e modais

--- pages/funcionariosadmin.tsx
+++ pages/funcionariosadmin.tsx
@@ -5,19 +5,82 @@ import { Footer1 } from '../src/components/Footer'
 import { Navbar1 } from '../src/components/Navbar'
 import { funcionarios } from "../api.json";
 import { TabelaAdmin } from '../src/components/TabelaAdmin'
+import { useState } from 'react';
+import NextImage from 'next/image'
 
 
 
+interface Funcionario {
+  id: number;
+  name: string;
+  email: string;
+  salario: number;
+  aniversario: string;
+  cargo: string;
+
+
+}
 
 export default function funcionariosadminPage() {
+  const [modalCriacaoAberto, setModalCriacaoAberto] = useState(false);
+
+  const ClickAddFuncionario = () => {
+    setModalCriacaoAberto(true);
+  };
+
   return (
     <>
     <div className='Titulopaginas'>Funcionários admin</div>
+    
     <div className='botaoaddfuncionario'>
 
-    <button> Adicionar Funcionario</button>
+    <button onClick={() =>ClickAddFuncionario()}> Adicionar Funcionario</button>
     </div>
+   
     <TabelaAdmin />
+    {modalCriacaoAberto && (
+  <div className="modal">
+    <div className="modal-conteudo">
+    <div className='Login_box login_boxfuncadmin'>
+    <div className='LogoDoSite'>
+                <NextImage src='/cloudservice.png' alt= 'Logo do Site' width={100} height={100} />
+            </div>
+            <form action='' method=''>
+                <div className='InputBoxesmodal'>
+                <div className='emailmodal modalprep'>
+
+                <label>Nome: </label>
+                <input type='name' name='nome'/>
+                </div>
+                  <div className='emailmodal modalprep'>
+
+                    <label>Email: </label>
+                    <input type='email' name='email'/>
+                  </div>
+                  <div className='salariomodal modalprep'>
+                      <label>Salário:</label>
+                    <input type='number' name='salario'/>
+                  </div>
+                  <div className='aniversariomodal modalprep'>
+                    <label>Aniversário:</label>
+                    <input type='text' name='aniversario'/>
+                  </div>
+                  <div className='cargomodal modalprep'>
+
+                    <label>Cargo:</label>
+                    <input type='text' name='cargo'/>
+                  </div>
+                    <div className='Botoes'>
+                        <button onClick={() => setModalCriacaoAberto(false)}className='Voltar' ><a href='#'> Fechar </a></button>
+                        <button ><a href='#'> Criar </a></button>
+                    </div>
+                </div>
+
+            </form>
+            </div>
+    </div>
+  </div>
+)}
     </>
 
 
--- src/components/TabelaAdmin/index.tsx
+++ src/components/TabelaAdmin/index.tsx
@@ -1,37 +1,163 @@
 import NextImage from 'next/image'
 import { funcionarios } from "../../../api.json";
-import { FaTrash, FaEdit, FaEye } from 'react-icons/fa';
-import { MdDelete, MdEdit, MdVisibility } from 'react-icons/md';
-import { FiTrash2, FiEdit2, FiEye } from 'react-icons/fi';
+import { MdEdit} from 'react-icons/md';
+import { FiTrash2, FiEye } from 'react-icons/fi';
+import { useState } from 'react';
 
 interface Funcionario {
     id: number;
     name: string;
     email: string;
+    salario: number;
+    aniversario: string;
+    cargo: string;
+
 
   }
-export function TabelaAdmin() {
-  return (
-<table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Gerenciamento</th>
-        </tr>
-      </thead>
-      <tbody>
-        {funcionarios.map((funcionario: Funcionario) => (
-          <tr key={funcionario.id}>
-            <td>{funcionario.id}</td>
-            <td>{funcionario.name}</td>
-            <td>{funcionario.email}</td>
-            <td><button><FiTrash2 size={30} color="#AC9479" /></button><button><MdEdit size={30} color="#AC9479" /></button><button><FiEye size={30} color="#AC9479" /></button></td>
-            
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+
+  export function TabelaAdmin() {
+    const [modalAberto, setModalAberto] = useState(false);
+    const [funcionarioSelecionado, FuncionarioSelecionado] = useState<Funcionario | null>(null);
+    const [modalEdicaoAberto, setModalEdicaoAberto] = useState(false);
+    const [modalDelete, setModalDeleteAberto] = useState(false);
+
+    const [funcionarioParaEdicao, setFuncionarioParaEdicao] = useState<Funcionario | null>(null);
+  
+    const ClickFiEye = (funcionario: Funcionario) => {
+      FuncionarioSelecionado(funcionario);
+      setModalAberto(true);
+    };
+
+    const ClickFiTrash2 = (funcionario: Funcionario) => {
+      FuncionarioSelecionado(funcionario);
+      setModalDeleteAberto(true);
+    };
+    const ClickMdEdit = (funcionario: Funcionario) => {
+      setFuncionarioParaEdicao(funcionario);
+      setModalEdicaoAberto(true);
+    };
+
+
+
+
+    
+
+
+  
+    return (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Gerenciamento</th>
+            </tr>
+          </thead>
+          <tbody>
+            {funcionarios.map((funcionario: Funcionario) => (
+              <tr key={funcionario.id}>
+                <td>{funcionario.id}</td>
+                <td>{funcionario.name}</td>
+                <td>{funcionario.email}</td>
+                <td>
+                  <button onClick={() => ClickFiTrash2(funcionario)}><FiTrash2 size={30} color="#AC9479" /></button>
+                  <button onClick={() =>ClickMdEdit(funcionario)}><MdEdit size={30} color="#AC9479" /></button>
+                  <button onClick={() => ClickFiEye(funcionario)}><FiEye size={30} color="#AC9479" /></button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {modalAberto && (
+          <div className="modal">
+            <div className="modal-conteudo">
+            <div className='Login_box login_boxfuncadmin'>
+            <div className='LogoDoSite'>
+                <NextImage src="/cloudservice.png" alt= 'Logo do Site' width={100} height={100} />
+            </div>
+            <div className='InputBoxesmodal'>
+              <h2 className='viewmodal h2viewmodal' >{funcionarioSelecionado.name}</h2>
+              <label htmlFor="">ID:</label>
+              <p className='viewmodal'> {funcionarioSelecionado.id}</p>
+              <label htmlFor="">Email:</label>
+              <p className='viewmodal'> {funcionarioSelecionado.email}</p>
+              <label htmlFor="">Salário:</label>
+              <p className='viewmodal'> {funcionarioSelecionado.salario}</p>
+              <label htmlFor="">Aniversário:</label>
+              <p className='viewmodal'> {funcionarioSelecionado.aniversario}</p>
+              <label htmlFor="">Cargo:</label>
+              <p className='viewmodal'> {funcionarioSelecionado.cargo}</p>
+              <button className='viewmodalbutton' onClick={() => setModalAberto(false)}>Fechar</button>
+            </div>
+            </div>
+          </div>
+          </div>
+
+        )}
+        {modalEdicaoAberto && (
+  <div className="modal">
+    <div className="modal-conteudo">
+    <div className='Login_box login_boxfuncadmin'>
+    <div className='LogoDoSite'>
+                <NextImage src='/cloudservice.png' alt= 'Logo do Site' width={100} height={100} />
+            </div>
+            <form action='' method=''>
+                <div className='InputBoxesmodal'>
+                  <div className='emailmodal modalprep'>
+
+                    <label>Email: </label>
+                    <input type='email' name='email'/>
+                  </div>
+                  <div className='salariomodal modalprep'>
+                      <label>Salário:</label>
+                    <input type='number' name='salario'/>
+                  </div>
+                  <div className='aniversariomodal modalprep'>
+                    <label>Aniversário:</label>
+                    <input type='text' name='aniversario'/>
+                  </div>
+                  <div className='cargomodal modalprep'>
+
+                    <label>Cargo:</label>
+                    <input type='text' name='cargo'/>
+                  </div>
+                    <div className='Botoes'>
+                        <button onClick={() => setModalEdicaoAberto(false)}className='Voltar' ><a href='#'> Fechar </a></button>
+                        <button ><a href='#'> Editar </a></button>
+                    </div>
+                </div>
+
+            </form>
+            </div>
+    </div>
+  </div>
+)}
+        {modalDelete && (
+  <div className="modal">
+    <div className="modal-conteudo">
+    <div className='Login_box login_boxfuncadmin'>
+    <div className='LogoDoSite'>
+                <NextImage src='/cloudservice.png' alt= 'Logo do Site' width={100} height={100} />
+            </div>
+            <form action='' method=''>
+                <div className='InputBoxesmodal'>
+                  <p className='confirmDelete'>Tem certeza que deseja deletar {funcionarioSelecionado.name}?</p>
+                    <div className='Botoes'>
+                        <button onClick={() => setModalDeleteAberto(false)}className='Voltar' ><a href='#'> Fechar </a></button>
+                        <button ><a href='#'> Deletar </a></button>
+                    </div>
+                </div>
+
+            </form>
+            </div>
+    </div>
+  </div>
+)}
+    
+
+
+      </>
+    );
+  }
