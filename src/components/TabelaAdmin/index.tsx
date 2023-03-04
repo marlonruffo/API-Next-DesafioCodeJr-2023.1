01f705a331d216c37e55dd326b6a576b1e9d224a
marlonruffo <ruffomarlon9@gmail.com>
2023-03-04 11:03:57 -0300

    adicionar e excluir(comproblemainda)

--- src/components/TabelaAdmin/index.tsx
+++ src/components/TabelaAdmin/index.tsx
@@ -1,5 +1,5 @@
 import NextImage from 'next/image'
-import { funcionarios } from "../../../api.json";
+import { funcionarios as funcionariosData} from "../../../api.json";
 import { MdEdit} from 'react-icons/md';
 import { FiTrash2, FiEye } from 'react-icons/fi';
 import { useState } from 'react';
@@ -16,10 +16,46 @@ interface Funcionario {
   }
 
   export function TabelaAdmin() {
+    const [modalCriacaoAberto, setModalCriacaoAberto] = useState(false)
+    const [novoFuncionario, setNovoFuncionario] = useState<Funcionario>({
+      id: 0,
+      name: '',
+      email: '',
+      salario: 0,
+      aniversario: '',
+      cargo: ''
+    })
     const [modalAberto, setModalAberto] = useState(false);
     const [funcionarioSelecionado, FuncionarioSelecionado] = useState<Funcionario | null>(null);
     const [modalEdicaoAberto, setModalEdicaoAberto] = useState(false);
     const [modalDelete, setModalDeleteAberto] = useState(false);
+    const [funcionarios, setFuncionarios] = useState<Funcionario[]>(funcionariosData);
+  
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target
+      setNovoFuncionario(prevState => ({
+        ...prevState,
+        [name]: value
+      }))
+    }
+  
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      const newFuncionario: Funcionario = {
+        ...novoFuncionario,
+        id: funcionarios.length + 1
+      }
+      funcionarios.push(newFuncionario)
+      setModalCriacaoAberto(false)
+      setNovoFuncionario({
+        id: 0,
+        name: '',
+        email: '',
+        salario: 0,
+        aniversario: '',
+        cargo: ''
+      })
+    }
 
     const [funcionarioParaEdicao, setFuncionarioParaEdicao] = useState<Funcionario | null>(null);
   
@@ -32,20 +68,30 @@ interface Funcionario {
       FuncionarioSelecionado(funcionario);
       setModalDeleteAberto(true);
     };
+
     const ClickMdEdit = (funcionario: Funcionario) => {
       setFuncionarioParaEdicao(funcionario);
       setModalEdicaoAberto(true);
     };
 
+    const DeleteFuncionario = (id: number) => {
+      const index = funcionarios.findIndex((funcionario) => funcionario.id === id);
+      if (index !== -1) {
+        const funcionariosAtualizados = [...funcionarios];
+        funcionariosAtualizados.splice(index, 1);
+        setFuncionarios(funcionariosAtualizados);
+      }
+    };
 
+    const ClickAddFuncionario = () => {
+      setModalCriacaoAberto(true)
+    }
 
-
-    
-
-
-  
     return (
       <>
+            <div className='botaoaddfuncionario'>
+        <button onClick={ClickAddFuncionario}> Adicionar Funcionario</button>
+      </div>
         <table>
           <thead>
             <tr>
@@ -145,8 +191,49 @@ interface Funcionario {
                 <div className='InputBoxesmodal'>
                   <p className='confirmDelete'>Tem certeza que deseja deletar {funcionarioSelecionado.name}?</p>
                     <div className='Botoes'>
-                        <button onClick={() => setModalDeleteAberto(false)}className='Voltar' ><a href='#'> Fechar </a></button>
-                        <button ><a href='#'> Deletar </a></button>
+                    <button onClick={() => setModalDeleteAberto(false)}className='Voltar' ><a > Fechar </a></button> 
+                    <button onClick={() => {DeleteFuncionario(funcionarioSelecionado.id);setModalDeleteAberto(false);}}><a > Deletar </a></button>          
+                    </div>
+                </div>
+
+            </form>
+            </div>
+    </div>
+  </div>
+)}
+{modalCriacaoAberto && (
+        <div className='modal'>
+          <div className='modal-conteudo'>
+            <div className='Login_box login_boxfuncadmin'>
+              <div className='LogoDoSite'>
+                <NextImage src='/cloudservice.png' alt='Logo do Site' width={100} height={100} />
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div className='InputBoxesmodal'>
+                  <div className='emailmodal modalprep'>
+                    <label>Nome: </label>
+                    <input type='name' name='name' value={novoFuncionario.name} onChange={handleChange} />
+                  </div>
+                  <div className='emailmodal modalprep'>
+                    <label>Email: </label>
+                    <input type='email' name='email' value={novoFuncionario.email} onChange={handleChange} />
+                  </div>
+                  <div className='salariomodal modalprep'>
+                    <label>Salário:</label>
+                    <input type='number' name='salario' value={novoFuncionario.salario} onChange={handleChange} />
+                  </div>
+                  <div className='aniversariomodal modalprep'>
+                    <label>Aniversário:</label>
+                    <input type='text' name='aniversario' value={novoFuncionario.aniversario} onChange={handleChange} />
+                  </div>
+                  <div className='cargomodal modalprep'>
+                    <label>Cargo:</label>
+
+                    <input type='text' name='cargo'value={novoFuncionario.cargo} onChange={handleChange} />
+                  </div>
+                    <div className='Botoes'>
+                        <button onClick={() => setModalCriacaoAberto(false)}className='Voltar' ><a > Fechar </a></button>
+                        <button type = 'submit'><a > Criar </a></button>
                     </div>
                 </div>
 
